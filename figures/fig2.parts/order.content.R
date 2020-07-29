c20b3923f4ad7c86d114150c45d15aee0c6382cf
Heath Blackmon <coleoguy@gmail.com>
2020-07-29 12:59:49 -0500

    hb changes

--- figures/fig2.parts/order.content.R
+++ figures/fig2.parts/order.content.R
@@ -2,18 +2,21 @@
 library(beeswarm)
 
 #load in microsatellite data
-dat.mic <- read.csv("../results/micRocounter_results_TII.csv")
+dat.mic <- read.csv("../../results/micRocounter_results_TII.csv")
 
-
-#create the beeswarm plot with the 
+orders <- sort(as.character(unique(dat.mic$order)))
+nums <- c(3, 12, 78, 1, 15, 68, 22, 2)
+labels <- paste(orders, " (", nums, ")", sep="")
+#create the beeswarm plot with the
 par(mar=c(7,4,4,4))
 beeswarm(log(dat.mic$bp.Mbp) ~ dat.mic$order, las = 2,
          xlab = "",
          ylab = "Microsatellite Content (bp/Mbp)",
          col = rgb(250, 159, 181, 100,
                    maxColorValue = 255), pch = 16,
+         labels=labels,
          method = "swarm", cex.main = .9,
-         cex = .95, spacing = .3, cex.lab = 1, cex.axis = 1
+         cex = .95, spacing = .3, cex.lab = 1, cex.axis = .8
 )
 
 #export as pdf 6" x 4"
--- figures/fig2.parts/order.rates.R
+++ figures/fig2.parts/order.rates.R
@@ -2,24 +2,27 @@
 library(beeswarm)
 
 #load in order rate data
-order.rates <- read.csv("../results/order.rates.csv")[,-1]
+order.rates <- read.csv("../../results/order.rates.csv")[,-1]
 
 #tidy up the data
-colnames(order.rates) <- c("Lepidoptera", "Hymneoptera", "Diptera", 
+colnames(order.rates) <- c("Lepidoptera", "Hymneoptera", "Diptera",
                            "Coleoptera", "Hemiptera")
 results <- data.frame(as.vector(unlist(order.rates)),
                       rep(colnames(order.rates), each = 100))
 colnames(results) <- c("rates", "order")
 
-#create the beeswarm plot with the 
+#create the beeswarm plot with the
 par(mar=c(7,4,4,4))
+raw.rates <- round(exp(c(-8, -6, -4, -2, 0, 2)), 3)
 beeswarm(log(results$rates) ~ results$order, las = 2,
          xlab = "",
          ylab = "Microsatellite Rates (bp change per MY)",
-         col = rgb(250, 159, 181, 100,
-                   maxColorValue = 255), pch = 16,
-         method = "swarm", cex.main = .9,
-         cex = .95, spacing = .3, cex.lab = 1, cex.axis = 1
+         col=rgb(250, 159, 181, 90, maxColorValue = 255),
+         pch = 16,
+         method = "swarm", cex.main = .9, yaxt="n",
+         cex = .65, spacing = .5, cex.lab = .8, cex.axis = 1
 )
+axis(at=c(-8, -6, -4, -2, 0, 2), labels=raw.rates, side=2,
+     las=1, cex.axis = .8)
 
 #export as pdf 6" x 6"
